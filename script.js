61aedaaddf2505b80b4e72b45497be5dea51f745
Javis-hub <ijave8richie@gmail.com>
2024-11-12 00:53:32 +0100

    update

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+
+
+
+
+
