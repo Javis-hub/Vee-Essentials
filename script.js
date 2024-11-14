dace56b71819644ce63058724c0c977ae1ed2d0f
Javis-hub <ijave8richie@gmail.com>
2024-11-14 19:12:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,66 @@ document.addEventListener('scroll', () => {
     }
 });
 
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
+accordionItemHeaders.forEach(accordionItemHeader => {
+   accordionItemHeader.addEventListener("click", event => {
+    
+     accordionItemHeader.classList.toggle("active");
+     const accordionItemBody = accordionItemHeader.nextElementSibling;
+     if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+     }
+     else {
+       accordionItemBody.style.maxHeight = 0;
+     }
+    
+   });
+});
 
+//seaech bar
 
+const itemList = [
+    "Bralette",
+];
 
+const resultBox = document.querySelector(
+    ".results"
+);
 
+const inputBox = document.querySelector(
+    ".search-bar"
+);
+
+const displayResults = function (result) {
+    const resultHTML = result.map(function (country) {
+        return `<li onclick=selectInput(this)>
+        ${items, products}
+        </li>`;
+    });
+
+    resultBpx.innerHTML = '<ul>' + resultHTML.join("") +   '</ul>';
+}
+
+inputBox.onkeyup = function (e) {
+    let result = [];
+
+    const input = input.value.toLowerCase();
+
+    if (input.length === 0) {
+        resultBox.innerHTML = "";
+    }
+
+    if (input.length === 0) {
+        result = itemList.filter((item) => {
+            return country.toLowerCase() .includes(input);
+        });
+
+        displayResults(result);
+    }
+}
+
+function selectInput(item) {
+    inputBox.value = country.innerText;
+    resultBox.innerHTML = "";
+};
